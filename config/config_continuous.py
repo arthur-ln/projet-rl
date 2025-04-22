04590ed6d51ed0c369a83b043cc9679da54ec629
Ilann AMIAUD--PLACHY <ilann.amiaudplachy@student-cs.fr>
2025-04-22 16:21:33 +0200

    changements intermédiaires

--- config/config_continuous.py
+++ config/config_continuous.py
@@ -1,64 +1,35 @@
-import pickle
-import gymnasium as gym
-import highway_env
-
-
 config_dict = {
-    "observation": {
-        "type": "Kinematics",
-        "vehicles_count": 8,
-        "features": [
-            "presence",
-            "x",
-            "y",
-            "vx",
-            "vy",
-            "cos_h",
-            "sin_h",
-        ],
-        "features_range": {
-            "x": [-100, 100],
-            "y": [-100, 100],
-            "vx": [-30, 30],
-            "vy": [-30, 30],
-        },
-        "absolute": True,
-        "normalize": True,
-    },
     "action": {
+        "lateral": True,
+        "longitudinal": True, # to True if acceleration wanted
+        'target_speeds': [0, 5, 10],
         "type": "ContinuousAction",
-        "longitudinal": True,  # Contrôle d'accélération
-        "lateral": True,  # Contrôle de direction
     },
-    "lanes_count": 3,
-    "vehicles_count": 10,
-    "duration": 80,  # [s]
-    "initial_spacing": 2,
-    "collision_reward": -2,
-    "right_lane_reward": 0.1,
-    "high_speed_reward": 0.4,
-    "lane_change_reward": -0.05,
-    "reward_speed_range": [
-        15,
-        25,
-    ],
-    "simulation_frequency": 10,  # [Hz]
-    "policy_frequency": 2,  # [Hz]
+    "action_reward": -0.1,
+    "centering_position": [0.5, 0.5],
+    "collision_reward": -10,
+    "controlled_vehicles": 1,
+    "duration": 300,
+    "lane_centering_cost": 4,
+    'lane_centering_reward': 1,
+    'manual_control': False,
+    "observation": {
+        "align_to_vehicle_axes": True,
+        "as_image": False,
+        "features": ['presence', 'on_road'],
+        "grid_size": [[-18, 18], [-18, 18]],
+        "grid_step": [3, 3],
+        "type": "OccupancyGrid",
+    },
+    "offscreen_rendering": True,
+    "other_vehicles": 7,
     "other_vehicles_type": "highway_env.vehicle.behavior.IDMVehicle",
-    "screen_width": 800,  # [px]
-    "screen_height": 200,  # [px]
-    "centering_position": [0.4, 0.5],
-    "scaling": 7.0,
-    "show_trajectories": True,
+    "policy_frequency": 5,
+    'real_time_rendering': False,
     "render_agent": True,
-    "offscreen_rendering": False,
-    "disable_collision_checks": False,
+    "scaling": 7,
+    "screen_height": 600,
+    "screen_width": 600,
+    "show_trajectories": True,
+    "simulation_frequency": 15,
 }
-
-with open("continuous_config.pkl", "wb") as f:
-    pickle.dump(config_dict, f)
-
-# Pour utiliser cette configuration:
-env = gym.make("highway-v0", render_mode="rgb_array")
-env.unwrapped.configure(config_dict)
-print(env.reset())
